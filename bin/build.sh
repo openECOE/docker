ee50be6bc6a599b0b8ffeedb992dba9cf8904546
Alberto Moreno <amoreno@umh.es>
2024-05-02 13:54:24 +0200

    Update build.sh script to include options for API_VERSION and WEBUI_VERSION

--- bin/build.sh
+++ bin/build.sh
@@ -10,7 +10,7 @@ WEBUI_VERSION="latest"
 ADDITIONAL_TAGS=()
 
 # Analizar las opciones y argumentos con nombre
-while getopts "r:R:t:" opt; do
+while getopts "r:R:t:a:w:" opt; do
   case $opt in
     r)
       REPO="$OPTARG"
@@ -21,21 +21,21 @@ while getopts "r:R:t:" opt; do
     t)
       ADDITIONAL_TAGS+=("$OPTARG")
       ;;
-    api)
+    a)
       API_VERSION="$OPTARG"
       ;;
-    web)
+    w)
       WEBUI_VERSION="$OPTARG"
       ;;
     *)
-      echo "Uso: $0 [-r repo] [-R release] [-api versionAPI] [-web versionWEBUI] [-t additional_tag]..."
+      echo "Uso: $0 [-r repo] [-R release] [-a versionAPI] [-w versionWEBUI] [-t additional_tag]..."
       exit 1
       ;;
   esac
 done
 
 # Construir la imagen de Docker
-docker build -t "$REPO:$RELEASE" --build-arg API_VERSION=$API_VERSION WEBUI_VERSION=$WEBUI_VERSION .
+docker build -t "$REPO:$RELEASE" --build-arg API_VERSION=$API_VERSION --build-arg WEBUI_VERSION=$WEBUI_VERSION .
 
 # Etiquetar la imagen de Docker
 for tag in "${ADDITIONAL_TAGS[@]}"; do
